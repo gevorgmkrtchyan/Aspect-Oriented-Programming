0f54cc42076fefc949ca52b6c7a1f83e7b5cb89f
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-08-06 20:25:46 +0400

    adding @AfterReturning Advice and checking post-process in the Aspect class

--- 4AOP-aspect-afterReturning/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 4AOP-aspect-afterReturning/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.example.aopdemo.aspect;
 
 import com.example.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,10 +10,39 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    //add an advice: AfterReturning for findAccounts() method
+    @AfterReturning(
+            pointcut = "execution(* com.example.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+        //print method name
+        String signature = joinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterReturning on method: " + signature);
+
+        //print result
+        System.out.println("\n=====>>> result is:" + result);
+
+        //let's post-process the data
+
+        //convert Account name to UPPERCASE
+        convertAccountNameToUpperCase(result);
+        System.out.println("\n=====>>> result is:" + result);
+    }
+
+    private void convertAccountNameToUpperCase(List<Account> result) {
+        for (Account account: result){
+            account.setName(account.getName().toUpperCase());
+        }
+    }
+
 
     @Before("com.example.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint) {
@@ -25,16 +55,15 @@ public class MyDemoLoggingAspect {
 
         //display method arguments
         Object[] args = joinPoint.getArgs();
-        for (Object arguments: args){
+        for (Object arguments : args) {
             System.out.println(arguments);
-            if (arguments instanceof Account){
-                Account account = (Account)arguments;
+            if (arguments instanceof Account) {
+                Account account = (Account) arguments;
                 System.out.println("Account name: " + account.getName());
                 System.out.println("Account level: " + account.getLevel());
             }
         }
     }
-
 
 
 }
